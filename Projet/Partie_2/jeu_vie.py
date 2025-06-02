aadc0bcd2322482e82be3c26bae57c54dcbb7ae8
Nathan CHOPIN <nathan.chopin76@gmail.com>
2025-06-02 14:24:42 +0200

    affichage et init de init_aleaztoire

--- Projet/Partie_2/jeu_vie.py
+++ Projet/Partie_2/jeu_vie.py
@@ -3,3 +3,21 @@
 # 2/06/25
 # TODO : tout
 
+from random import randint
+
+vivant = '◼️'
+mort ='◻️'
+N = 15
+T = [[ '◻️' ] * N]*N
+
+def affichage():
+    '''permet d'afficher le plateau'''
+    ligne = ''
+    for i in T:
+        for k in i:
+            ligne += k + ' '
+        print(ligne)
+        ligne = ''
+
+def init_aleatoire():
+    return
