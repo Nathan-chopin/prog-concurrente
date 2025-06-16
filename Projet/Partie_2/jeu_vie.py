e286e40267d288a89570be8142b8a3c6bd607372
Nathan CHOPIN <nathan.chopin76@gmail.com>
2025-06-16 16:48:07 +0200

    avancement

--- Projet/Partie_2/jeu_vie.py
+++ Projet/Partie_2/jeu_vie.py
@@ -36,7 +36,7 @@ def init_aleatoire():
             if randint(0,8) == 1:
                 T[i][k] = vivant
 
-def vie_mort(x,y,etat):
+def vie_mort(lock,x,y,etat):
     bord_x0  = x == 0
     bord_x14 = x == len(T)
     bord_y0  = y == 0
@@ -44,23 +44,34 @@ def vie_mort(x,y,etat):
 
     nb_mort =  3 * bord_x0  + (3 * bord_y0 - int(bord_y0 and bord_x0 )) + (3 * bord_y14 - int(bord_y14 and bord_x0 ))
     nb_mort += 3 * bord_x14 + (3 * bord_y0 - int(bord_y0 and bord_x14)) + (3 * bord_y14 - int(bord_y14 and bord_x14))
-
-    if etat == mort:
-        return
+    
+    for i in [int(not bord_y0),-1 * int(not bord_y14)]:
+        if i != 0:
+            if not bord_x0:
+                nb_mort += int(T[x-1][y+i] == mort)
+            if not bord_x14:
+                nb_mort += int(T[x+1][y+i] == mort)
+            nb_mort += int(T[x][y+i] == mort)
+    
+    nb_vivant = 8 - nb_mort
+    if etat == mort and nb_vivant == 3:
+        with lock:
+            T[x][y] = vivant
+        
 
 def action_clavier(event):
     if event.name == 'b':
         print('caca')
 
 if __name__ == '__main__' :
-
+    lock = mp.Lock()
     init_aleatoire()
     affichage()
     
     mes_process = []                          # Liste des processus chevaux
     for x in range(N):
         for y in range(N):
-            p = mp.Process(target=vie_mort, args=(x,y,T[x][y]))  # Crée le processus cheval
+            p = mp.Process(target=vie_mort, args=(lock,x,y,T[x][y]))  # Crée le processus cheval
             p.start()                             # Lance le processus
             mes_process.append(p)
 
